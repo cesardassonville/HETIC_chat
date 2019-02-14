6660734964862c73bbd8a4ed60189eb2fb7bd707
cesardassonville <cesar.cofh@hotmail.com>
2019-02-14 11:49:07 +0100

    maj4

--- server.js
+++ server.js
@@ -1,13 +1,21 @@
 /*
 Imports
 */
+
 //
 
 /*Configuration
 */
+let express = require('express');
+let port = 8080;
+let app = express();
 //
 
 /*
 Démarrage
 */
+app.listen(port, () => {
+    console.log('serveur started on port ' + port);
+});
 //
+
